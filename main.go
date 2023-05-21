5df9dece939a41dfdc4e615b894761e464084aed
DUYGU SEYHAN <duygumersin5@gmail.com>
2023-05-21 15:39:45 +0300

    folders

--- main.go
+++ main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-
-type Customer struct {
-	ID        uint      `gorm:"primarykey"`
-	FirstName string    `gorm:"type:varchar(100);not null"`
-	LastName  string    `gorm:"type:varchar(100);not null"`
-	BirthDate time.Time `gorm:"not null"`
-	Gender    string    `gorm:"type:varchar(6);not null"`
-	Email     string    `gorm:"type:varchar(100);not null;unique"`
-	Address   string    `gorm:"type:varchar(200)"`
-}
 
 func main() {
 	dsn := "host=localhost user=postgres password=password dbname=mydb port=5432 sslmode=disable"
